fae6597cb8706a35495fd7361e8eb6782ae82541
Ali mohsenPour <86446834+alimpp@users.noreply.github.com>
2022-11-19 12:37:54 +0330

    add express project

---
+++ app.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const users = require('./users')
+const app = express()
+const { body, validationResult } = require('express-validator')
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({
+   extended: true
+}));
+app.use(bodyParser.json())
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+   res.send("Hello Express Js")
+})
+
+app.get('/api/users', (req, res) => {
+   res.json({ data: users, message: "ok" })
+})
+
+app.get('/api/user/:id', (req, res) => {
+   const user = users.find(u => u.id == req.params.id)
+   res.json(user)
+})
+
+app.post('/api/user/create', [
+   body('email', 'email has error').isEmail(),
+   body('name', 'name must be empty').notEmpty(),
+], (req, res) => {
+   const errors = validationResult(req.body)
+   if (!errors.isEmpty()) {
+      return res.status(400).json({ data: null, errors: errors.array(), message: 'validation errors' })
+   }
+   users.push({ id: users.length + 1, ...req.body })
+   res.json({
+      data: users,
+      message: "ok",
+   })
+})
+
+const port = process.env.PORT || 3000
+
+app.listen(port, (err) => {
+   if (err) console.log(err);
+   console.log(`${port} conection`);
+})
